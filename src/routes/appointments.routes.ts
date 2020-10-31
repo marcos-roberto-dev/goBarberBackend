c6607857202c6a3332cd017d4c6c0353277d367f
Marcos Roberto <marcosprojetosdev@gmail.com>
2020-10-31 00:56:57 -0300

    :art: refactor appointments.routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,7 +5,6 @@ import CreateAppointmenteService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
-const createAppointment = new CreateAppointmenteService(appointmentsRepository);
 
 appointmentsRouter.get('/', (request, response) => {
   const appointments = appointmentsRepository.all();
@@ -15,7 +14,13 @@ appointmentsRouter.get('/', (request, response) => {
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
   const parsedDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmenteService(
+    appointmentsRepository,
+  );
+
   const appointment = createAppointment.execute({ date: parsedDate, provider });
+
   return response.json(appointment);
 });
 
